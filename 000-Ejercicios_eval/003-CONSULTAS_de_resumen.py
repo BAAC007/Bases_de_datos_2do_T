0c0581e3b97805fac893b68f1682ed9076252936
Bryan Avila <148177252+BAAC007@users.noreply.github.com>
2025-12-08 19:25:26 +0100

    update

--- 000-Ejercicios_eval/003-CONSULTAS_de_resumen.py
+++ 000-Ejercicios_eval/003-CONSULTAS_de_resumen.py
@@ -19,6 +19,15 @@ filas = cursor.fetchall()
 
 print(filas)
 
+edades = [22, 19, 25, 20, 23, 21, 24, 25, 18, 20, 22, 24, 17, 19, 23, 25, 21, 18, 20, 24]
+
+etiquetas = [
+    'Edad 22', 'Edad 19', 'Edad 25', 'Edad 20', 'Edad 23',
+    'Edad 21', 'Edad 24', 'Edad 25', 'Edad 18', 'Edad 20',
+    'Edad 22', 'Edad 24', 'Edad 17', 'Edad 19', 'Edad 23',
+    'Edad 25', 'Edad 21', 'Edad 18', 'Edad 20', 'Edad 24'
+]
+
 pt.pie(edades, labels=etiquetas, autopct='%1.1f%%')
 pt.axis('equal')  # Para que el gráfico sea circular
 pt.show()
