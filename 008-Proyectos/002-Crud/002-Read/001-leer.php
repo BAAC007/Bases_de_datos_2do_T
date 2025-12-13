c1b89e7869d36b42db85ee4f424159222faaf2c7
Bryan Avila <bc714006@gmail.com>
2025-12-13 13:23:04 +0100

    update

--- 008-Proyectos/002-Crud/002-Read/001-leer.php
+++ 008-Proyectos/002-Crud/002-Read/001-leer.php
@@ -9,24 +9,15 @@
   
 	// Comprobacion exitosa pero mirando los datos que vienen del formulario en POST
   $sql = "
-  	SELECT 
-    *
-    FROM usuarios
-    WHERE
-    usuario = '".$_POST['usuario']."'
-    AND
-    contrasena = '".$_POST['contrasena']."';
+  	SELECT * FROM empleados;
   ";
 	
   $resultado = $conexion->query($sql);
-
-  if ($fila = $resultado->fetch_assoc()) {	// Si es cierto que hay un resultado
-    $_SESSION['usuario'] = 'si';
-    header("Location: exito.php");					// En ese caso vamos a la pagina de exito
-  }else{																		// Si no hay ningun resultado
-  	header("Location: login.html");					// En ese caso volvemos al login
+  while ($fila = $resultado->fetch_assoc()) {
+  	var_dump($fila);			// Vomito en pantalla y ya luego formateare
   }
-
+	
+  	
   $conexion->close();
   
 ?>
